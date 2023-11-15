67ebf537a13e57894d9868649b7754ec244b575f
Anisha Putri Qonitah <nishaputriq@gmail.com>
2023-11-15 14:46:56 +0700

    tutorial 8

--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
 
 class ShopItem {
